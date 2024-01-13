ff829a70ef0966565d4251fc6b93e3e5d62c97e6
Ngoc Le <ngocle@rtanalytics.vn>
2024-01-13 22:16:01 +0700

    validate data

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,14 @@
+import { IsEmail, IsNotEmpty } from "class-validator"
+
 export class CreateUserDto {
-    name: string
-    email: string
-    password: string
-    address: string
+    @IsNotEmpty({message: 'Name không được để trống'})
+    name: string;
+
+    @IsNotEmpty({message: 'Email không được để trống'})
+    @IsEmail({},{message: 'Email không đúng định dạng'})
+    email: string;
+
+    @IsNotEmpty({message: 'Password không được để trống'})
+    password: string;
+    address: string;
 }

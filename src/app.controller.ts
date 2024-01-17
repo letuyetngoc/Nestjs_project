1f1e4e8683a4b19e78c9dbab68311b514dd1cbc3
Ngoc Le <ngocle@rtanalytics.vn>
2024-01-17 08:32:42 +0700

    disable guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,19 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './decorator/customize';
 @Controller()
 export class AppController {
   constructor(
     private authService: AuthService
   ) { }
 
-  // @UseGuards(LocalAuthGuard)
+  @Public()
+  @UseGuards(LocalAuthGuard)
   @Post('/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
-
   // @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {

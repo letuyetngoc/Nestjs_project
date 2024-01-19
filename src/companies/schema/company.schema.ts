80592fce202cfe769a7c9237286c5a0beaf6e214
Ngoc Le <ngocle@rtanalytics.vn>
2024-01-19 14:42:04 +0700

    generate Modules Company

---
+++ src/companies/schema/company.schema.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type CompanyDocument = HydratedDocument<Company>;
+
+@Schema()
+export class Company {
+    @Prop({required:true})
+    name: string;
+
+    @Prop({required:true})
+    address: string;
+
+    @Prop({required:true})
+    description: string;
+
+    @Prop()
+    createdAt: Date;
+
+    @Prop()
+    updatedAt: Date;
+
+    @Prop()
+    deleteddAt: Date;
+
+    @Prop()
+    isDeleted: boolean;
+
+    @Prop({type:Object})
+    createdBy: {
+        _id: mongoose.Schema.Types.ObjectId,
+        email: string
+    };
+
+    @Prop({type:Object})
+    updatedBy: {
+        _id: mongoose.Schema.Types.ObjectId,
+        email: string
+    };
+
+    @Prop({type:Object})
+    deletedBy: {
+        _id: mongoose.Schema.Types.ObjectId,
+        email: string
+    };
+}
+
+export const CompanySchema = SchemaFactory.createForClass(Company);

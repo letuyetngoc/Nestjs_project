fa9da58bf0b8224460ae13b9f3e30f5f851cf934
Ngoc Le <ngocle@rtanalytics.vn>
2024-02-26 21:58:27 +0700

    config module mail

---
+++ src/mail/mail.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Public, ResponseMessage } from 'src/decorator/customize';
+
+@Controller('mail')
+export class MailController {
+  constructor(
+    private readonly mailService: MailService,
+    private mailerService: MailerService
+  ) { }
+
+  @Get()
+  @Public()
+  @ResponseMessage("Test email")
+  async handleTestEmail() {
+    await this.mailerService.sendMail({
+      to: "letuyetngocksh@gmail.com",
+      from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Welcome to Nice App! Confirm your Email',
+      html: '<b>welcome bla bla</b>', // HTML body content
+    });
+
+  }
+}

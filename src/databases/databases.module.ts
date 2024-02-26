9f7e4e645ac619c3026c6913fdde55420f395a67
Ngoc Le <ngocle@rtanalytics.vn>
2024-02-26 14:51:11 +0700

    fake database

--- src/databases/databases.module.ts
+++ src/databases/databases.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
 import { DatabasesService } from './databases.service';
 import { DatabasesController } from './databases.controller';
-import { User } from 'src/decorator/customize';
-import { UserSchema } from 'src/users/schemas/user.schema';
+import { User, UserSchema } from 'src/users/schemas/user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Permission, PermissionSchema } from 'src/permissions/schema/permission.schema';
 import { Role, RoleSchema } from 'src/roles/schema/role.schema';
 import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([
-    { name: User.name, schema: UserSchema },
-    { name: Permission.name, schema: PermissionSchema },
-    { name: Role.name, schema: RoleSchema },
-  ])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Permission.name, schema: PermissionSchema },
+      { name: Role.name, schema: RoleSchema }
+    ])
+  ],
   controllers: [DatabasesController],
   providers: [DatabasesService, UsersService]
 })

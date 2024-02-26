23c745368765691fe5b110b14e4161d266562649
Ngoc Le <ngocle@rtanalytics.vn>
2024-02-26 11:31:53 +0700

    update a role

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Patch, Param } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { ResponseMessage, User } from 'src/decorator/customize';
 import { IUser } from 'src/users/schemas/user.interface';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Controller('roles')
 export class RolesController {
@@ -11,6 +12,15 @@ export class RolesController {
   @ResponseMessage('Create a new Role')
   create(@Body() createRoleDto: CreateRoleDto, @User() user: IUser) {
     return this.rolesService.create(createRoleDto, user);
+  }
+
+  @Patch(':id')
+  @ResponseMessage('Update a role')
+  update(
+    @Body() updateRolesDto: UpdateRoleDto,
+    @Param() id: string,
+    @User() user: IUser) {
+    return this.rolesService.update(updateRolesDto, user, id);
   }
   
 }
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -4,6 +4,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Role, RoleDocument } from './schema/role.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { IUser } from 'src/users/schemas/user.interface';
+import { UpdateRoleDto } from './dto/update-role.dto';
+import mongoose from 'mongoose';
 
 @Injectable()
 export class RolesService {
@@ -29,5 +31,13 @@ export class RolesService {
             _id: newPermission._id,
             createdAt: newPermission.createdAt
         }
+    }
+
+    //update a role
+    async update(updateRoleDto: UpdateRoleDto, user: IUser, id: string) {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            throw new BadRequestException('not found permission')
+        }
+        return this.roleModel.updateOne({ ...updateRoleDto, updatedBy: { _id: user._id, email: user.email } })
     }
 }

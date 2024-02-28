fd346793784bed51805dbedbf2f2d4e785643504
Ngoc Le <ngocle@rtanalytics.vn>
2024-02-28 18:16:01 +0700

    config static file public

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,24 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 import cookieParser from 'cookie-parser';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
 
   app.useGlobalPipes(new ValidationPipe({
-    whitelist:true
+    whitelist: true
   }));
 
   app.useGlobalInterceptors(new TransformInterceptor(reflector));
